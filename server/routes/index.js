8aa9da93c7af8c0d75a83f727f5909a35069a339
jibaru <ignacioruedaboada@hotmail.com>
2020-05-03 16:50:52 -0500

    Added Login route to index

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+app.use(require('./login.js'));
 app.use(require('./user.js'));
 app.use(require('./recipe.js'));
 app.use(require('./ingredient.js'));
