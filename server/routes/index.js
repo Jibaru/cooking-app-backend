3e6fedba5f03089e65c38136efffa954ba5df476
jibaru <ignacioruedaboada@hotmail.com>
2020-05-03 21:04:56 -0500

    Added signin route to index

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(require('./login.js'));
+app.use(require('./signin'));
 app.use(require('./user.js'));
 app.use(require('./recipe.js'));
 app.use(require('./ingredient.js'));
