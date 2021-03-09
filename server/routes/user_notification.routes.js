17bb5094b420a1ae0f3bba6412647d414e5e4cb0
Jibaru <ignacioruedaboada@hotmail.com>
2021-03-08 20:52:55 -0500

    Added get all and update notification routes

--- server/routes/user_notification.routes.js
+++ server/routes/user_notification.routes.js
@@ -1,17 +1,35 @@
-const app = require('express')();
-const validateErrors = require('../middlewares/validate_errors.middleware');
-const { 
-    createUserNotificationMiddleware,
-    deleteUserNotificationMiddleware
-} = require('../middlewares/user_notification.middlewares');
+const app = require("express")();
+const validateErrors = require("../middlewares/validate_errors.middleware");
+const {
+  createUserNotificationMiddleware,
+  deleteUserNotificationMiddleware,
+  getAllUserNotificationsMiddleware,
+  updateUserNotificationMiddleware,
+} = require("../middlewares/user_notification.middlewares");
 
 /// UserNotification Services
-app.post('/user-notifications',
-    [createUserNotificationMiddleware, validateErrors],
-    require('../controllers/user_notification/create.controller'));
+app.get(
+  "/user-notifications",
+  [getAllUserNotificationsMiddleware, validateErrors],
+  require("../controllers/user_notification/get_all.controller")
+);
 
-app.delete('/user-notifications/:id',
-    [deleteUserNotificationMiddleware, validateErrors],
-    require('../controllers/user_notification/delete.controller'));
+app.post(
+  "/user-notifications",
+  [createUserNotificationMiddleware, validateErrors],
+  require("../controllers/user_notification/create.controller")
+);
+
+app.put(
+  "/user-notifications/:id",
+  [updateUserNotificationMiddleware, validateErrors],
+  require("../controllers/user_notification/update.controller")
+);
+
+app.delete(
+  "/user-notifications/:id",
+  [deleteUserNotificationMiddleware, validateErrors],
+  require("../controllers/user_notification/delete.controller")
+);
 
 module.exports = app;
