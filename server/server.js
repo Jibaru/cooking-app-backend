99e03468f3958a67e858906470127569148f5f0e
Ignacio <ignacioruedaboada@hotmail.com>
2021-03-09 11:00:02 -0500

    Changed environment to dotenv

--- server/server.js
+++ server/server.js
@@ -3,22 +3,22 @@
 /// ===================================================
 
 // Enviroment process
-require('./config/enviroment');
+require("dotenv").config();
 
 // Body Parser
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 // Express server
-const app = require('express')();
+const app = require("express")();
 
 // BodyParser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Routes middleware
-app.use(require('./routes/index'));
+app.use(require("./routes/index"));
 
 // Run server
 app.listen(process.env.PORT, () => {
-	console.log(`Escuchando en el puerto ${process.env.PORT}`);
+  console.log(`Escuchando en el puerto ${process.env.PORT}`);
 });
