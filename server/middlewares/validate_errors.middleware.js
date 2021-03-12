65783adebbf0f1fe2c062f73586bfc5036fc6515
Ignacio <ignacioruedaboada@hotmail.com>
2021-03-11 21:37:48 -0500

    Formatted file

--- server/middlewares/validate_errors.middleware.js
+++ server/middlewares/validate_errors.middleware.js
@@ -1,26 +1,24 @@
-const { validationResult } = require('express-validator');
-const { clientError } = require('../utils/http_status_codes');
-const { fromExpressValidator } = require('../utils/error_formatter');
+const { validationResult } = require("express-validator");
+const { clientError } = require("../utils/http_status_codes");
+const { fromExpressValidator } = require("../utils/error_formatter");
 
 /// Validate errors from validation schemas
 const validateErrors = (req, res, next) => {
+  const expressValidatorErrors = validationResult(req)
+    .array()
+    .map((error) => fromExpressValidator(error));
+  const customErrors = req.errors || [];
 
-    const expressValidatorErrors = 
-        validationResult(req)
-        .array()
-        .map(error => fromExpressValidator(error));
-    const customErrors = req.errors || [];
+  const allErrors = [...expressValidatorErrors, ...customErrors];
 
-    const allErrors = [...expressValidatorErrors, ...customErrors];
-
-    if(allErrors.length > 0) {
-        return res.status(clientError.badRequest).json({
-            ok: false,
-            errors: allErrors
-        });
-    } else {
-        next();
-    }
-}
+  if (allErrors.length > 0) {
+    return res.status(clientError.badRequest).json({
+      ok: false,
+      errors: allErrors,
+    });
+  } else {
+    next();
+  }
+};
 
 module.exports = validateErrors;
