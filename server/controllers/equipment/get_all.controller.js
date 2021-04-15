1a04a133f8fdd8a3e8215bbf78e2ec7ecabc5fa2
jibaru <xxnacho1997xx@gmail.com>
2021-04-15 11:36:47 -0500

    Added filters

--- server/controllers/equipment/get_all.controller.js
+++ server/controllers/equipment/get_all.controller.js
@@ -1,15 +1,101 @@
 const { toResponseFormat } = require("../../utils/response_formatter");
 const { success, clientError } = require("../../utils/http_status_codes");
-const { Equipment } = require("../../db/models/index");
+const { Equipment, Sequelize, Instruction } = require("../../db/models/index");
+const PaginationConstants = require("../../constants/pagination.contants");
 
 // Get all Equipments
 const getAllController = (req, res) => {
-  Equipment.findAll()
-    .then((equipments) => equipments.map((e) => toResponseFormat(e.toJSON())))
+  const Op = Sequelize.Op;
+
+  const {
+    name,
+    status,
+    recipeIds,
+    fromCreatedAt,
+    toCreatedAt,
+    page = PaginationConstants.DEFAULT_PAGE,
+    pageSize = PaginationConstants.PAGE_SIZE,
+  } = req.query;
+
+  Equipment.findAll({
+    attributes: {
+      include: ["createdAt"],
+    },
+    include: [
+      {
+        model: Instruction,
+        as: "instructions",
+        attributes: {
+          include: ["id"],
+        },
+      },
+    ],
+    where: {
+      // Filter name%
+      ...(name && {
+        name: {
+          [Op.startsWith]: name,
+        },
+      }),
+      // Filter status
+      ...(status && {
+        status: {
+          [Op.eq]: status,
+        },
+      }),
+      // Filter from X to Y createdAt
+      ...((fromCreatedAt || toCreatedAt) && {
+        createdAt: {
+          ...(fromCreatedAt && {
+            [Op.gte]: fromCreatedAt,
+          }),
+          ...(toCreatedAt && {
+            [Op.lte]: toCreatedAt,
+          }),
+        },
+      }),
+    },
+    order: [["createdAt", "DESC"]],
+  })
     .then((equipments) => {
+      // Check if an equipment has all recipeIds
+      // If recipeIds is empty or not exists, return true
+      function isInAllRecipes(instructions) {
+        if (!recipeIds) {
+          return true;
+        }
+        const instructionIds = instructions.map(({ id }) => id);
+        return recipeIds.every((recipeId) => instructionIds.includes(recipeId));
+      }
+
+      const filteredEquipments = equipments.filter((equipment) =>
+        isInAllRecipes(equipment.instructions)
+      );
+
+      const start = page * pageSize - pageSize;
+      const end = offset + pageSize;
+
+      return {
+        count: filteredEquipments.length,
+        rows: filteredEquipments.slice(start, end),
+      };
+    })
+    .then(({ count, rows }) => {
+      return {
+        totalEquipments: count,
+        equipments: rows.map((e) => toResponseFormat(e.toJSON())),
+      };
+    })
+    .then(({ totalEquipments, equipments }) => {
+      const totalPages = Math.ceil(totalEquipments / pageSize);
+
       return res.status(success.ok).json({
         ok: true,
         equipments,
+        totalPages,
+        currentPage: page,
+        pageSize,
+        totalCount: totalEquipments,
       });
     })
     .catch((error) => {
