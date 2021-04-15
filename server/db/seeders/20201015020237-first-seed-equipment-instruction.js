a1b41f7b3404c75877ea1b266980edf56e5d93a1
jibaru <xxnacho1997xx@gmail.com>
2021-04-15 10:54:27 -0500

    Added equipmentInstruction seeders

--- server/db/seeders/20201015020237-first-seed-equipment-instruction.js
+++ server/db/seeders/20201015020237-first-seed-equipment-instruction.js
@@ -9,6 +9,98 @@ module.exports = {
           equipmentId: 1,
           instructionId: 1,
         },
+        {
+          equipmentId: 1,
+          instructionId: 2,
+        },
+        {
+          equipmentId: 1,
+          instructionId: 4,
+        },
+        {
+          equipmentId: 2,
+          instructionId: 1,
+        },
+        {
+          equipmentId: 2,
+          instructionId: 3,
+        },
+        {
+          equipmentId: 3,
+          instructionId: 2,
+        },
+        {
+          equipmentId: 3,
+          instructionId: 5,
+        },
+        {
+          equipmentId: 4,
+          instructionId: 1,
+        },
+        {
+          equipmentId: 4,
+          instructionId: 2,
+        },
+        {
+          equipmentId: 4,
+          instructionId: 3,
+        },
+        {
+          equipmentId: 4,
+          instructionId: 4,
+        },
+        {
+          equipmentId: 5,
+          instructionId: 1,
+        },
+        {
+          equipmentId: 6,
+          instructionId: 3,
+        },
+        {
+          equipmentId: 6,
+          instructionId: 5,
+        },
+        {
+          equipmentId: 7,
+          instructionId: 1,
+        },
+        {
+          equipmentId: 7,
+          instructionId: 2,
+        },
+        {
+          equipmentId: 7,
+          instructionId: 3,
+        },
+        {
+          equipmentId: 7,
+          instructionId: 4,
+        },
+        {
+          equipmentId: 8,
+          instructionId: 1,
+        },
+        {
+          equipmentId: 8,
+          instructionId: 2,
+        },
+        {
+          equipmentId: 8,
+          instructionId: 3,
+        },
+        {
+          equipmentId: 9,
+          instructionId: 1,
+        },
+        {
+          equipmentId: 9,
+          instructionId: 2,
+        },
+        {
+          equipmentId: 9,
+          instructionId: 4,
+        },
       ],
       {}
     );
