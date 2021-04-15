6d2b7481145531167aa4084f8f4d2235e1631f62
jibaru <xxnacho1997xx@gmail.com>
2021-04-15 10:52:50 -0500

    Added equipment seeders

--- server/db/seeders/20201015020226-first-seed-equipment.js
+++ server/db/seeders/20201015020226-first-seed-equipment.js
@@ -6,9 +6,49 @@ module.exports = {
       "Equipments",
       [
         {
-          imageId: 4,
+          imageId: 1,
           name: "LICUADORA",
           description: "Para licuar ingredientes",
+        },
+        {
+          imageId: 2,
+          name: "BATIDORA",
+          description: "Bate los ingredientes",
+        },
+        {
+          imageId: 3,
+          name: "RAYADORA",
+          description: "Raya ingredientes",
+        },
+        {
+          imageId: 4,
+          name: "CUCHARÓN",
+          description: "Cuchara grande",
+        },
+        {
+          imageId: 5,
+          name: "OLLA ARROCERA",
+          description: "Permite hacer arroz",
+        },
+        {
+          imageId: 6,
+          name: "OLLA DE BARRO",
+          description: "Olla de barro",
+        },
+        {
+          imageId: 7,
+          name: "PARRILLA",
+          description: "Para asar la carne",
+        },
+        {
+          imageId: 8,
+          name: "ESPATULA",
+          description: "Espátula de cocina",
+        },
+        {
+          imageId: 9,
+          name: "AMASADORA",
+          description: "Permite amasar",
         },
       ],
       {}
