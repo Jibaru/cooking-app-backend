037fe38b6bfe8a1f7f2dca7206052cf56426c907
jibaru <xxnacho1997xx@gmail.com>
2021-04-15 10:52:15 -0500

    Added instruction seeders

--- server/db/seeders/20201015020220-first-seed-instruction.js
+++ server/db/seeders/20201015020220-first-seed-instruction.js
@@ -2,7 +2,30 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert("Instructions", [{}], {});
+    return queryInterface.bulkInsert(
+      "Instructions",
+      [
+        {
+          id: 1,
+        },
+        {
+          id: 2,
+        },
+        {
+          id: 3,
+        },
+        {
+          id: 4,
+        },
+        {
+          id: 5,
+        },
+        {
+          id: 6,
+        },
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
