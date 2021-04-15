d537a210343034250326cd187460107187d9ec5a
jibaru <xxnacho1997xx@gmail.com>
2021-04-15 10:54:44 -0500

    Added ingredient seeders

--- server/db/seeders/20201015020247-first-seed-ingredient.js
+++ server/db/seeders/20201015020247-first-seed-ingredient.js
@@ -6,9 +6,49 @@ module.exports = {
       "Ingredients",
       [
         {
-          imageId: 3,
+          imageId: 1,
           name: "CARNE",
           description: "Carne suave",
+        },
+        {
+          imageId: 2,
+          name: "POLLO",
+          description: "Pollo suave",
+        },
+        {
+          imageId: 3,
+          name: "TOMATE",
+          description: "Tomate rojo del Perú",
+        },
+        {
+          imageId: 4,
+          name: "CEBOLLA",
+          description: "Cebolla roja",
+        },
+        {
+          imageId: 5,
+          name: "PAPA AMARILLA",
+          description: "Papa amarilla de Huanvelica",
+        },
+        {
+          imageId: 6,
+          name: "CHILE",
+          description: "Chile mexicano",
+        },
+        {
+          imageId: 7,
+          name: "PASTA",
+          description: "Pasta italiana",
+        },
+        {
+          imageId: 8,
+          name: "ARROZ",
+          description: "Arroz norteño",
+        },
+        {
+          imageId: 9,
+          name: "HUEVO",
+          description: "Huevo de gallina",
         },
       ],
       {}
