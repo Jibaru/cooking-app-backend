2ad23ba71e414371cf10e21c89599e9dcb48adc8
jibaru <xxnacho1997xx@gmail.com>
2021-04-16 15:48:58 -0500

    Added filters

--- server/controllers/recipe_cuisine/get_all.controller.js
+++ server/controllers/recipe_cuisine/get_all.controller.js
@@ -1,17 +1,99 @@
 const { toResponseFormat } = require("../../utils/response_formatter");
 const { success, clientError } = require("../../utils/http_status_codes");
-const { RecipeCuisine } = require("../../db/models/index");
+const { RecipeCuisine, Sequelize, Recipe } = require("../../db/models/index");
+const PaginationConstants = require("../../constants/pagination.contants");
 
 /// Get all RecipeCuisines
 const getAllController = (req, res) => {
-  RecipeCuisine.findAll()
-    .then((recipeCuisines) =>
-      recipeCuisines.map((e) => toResponseFormat(e.toJSON()))
-    )
+  const Op = Sequelize.Op;
+
+  const {
+    name,
+    region,
+    recipeIds,
+    fromCreatedAt,
+    toCreatedAt,
+    page = PaginationConstants.DEFAULT_PAGE,
+    pageSize = PaginationConstants.PAGE_SIZE,
+  } = req.query;
+
+  RecipeCuisine.findAll({
+    attributes: {
+      include: ["createdAt"],
+    },
+    include: [
+      {
+        model: Recipe,
+        as: "recipes",
+        attributes: {
+          include: ["id"],
+        },
+      },
+    ],
+    where: {
+      // Filter name%
+      ...(name && {
+        name: {
+          [Op.startsWith]: name,
+        },
+      }),
+      // Filter region
+      ...(region && {
+        region: {
+          [Op.eq]: region,
+        },
+      }),
+      // Filter from X to Y createdAt
+      ...((fromCreatedAt || toCreatedAt) && {
+        createdAt: {
+          ...(fromCreatedAt && {
+            [Op.gte]: fromCreatedAt,
+          }),
+          ...(toCreatedAt && {
+            [Op.lte]: toCreatedAt,
+          }),
+        },
+      }),
+    },
+    order: [["createdAt", "DESC"]],
+  })
     .then((recipeCuisines) => {
+      function isInAllRecipes(recipes) {
+        if (!recipeIds) {
+          return true;
+        }
+        const queryRecipeIds = recipes.map(({ id }) => id);
+        return recipeIds.every((recipeId) => queryRecipeIds.includes(recipeId));
+      }
+
+      const filteredRecipeCuisines = recipeCuisines.filter((recipeCuisine) =>
+        isInAllRecipes(recipeCuisine.recipes)
+      );
+
+      const start = page * pageSize - pageSize;
+      const end = start + pageSize;
+
+      return {
+        count: filteredRecipeCuisines.length,
+        rows: filteredRecipeCuisines.slice(start, end),
+      };
+    })
+    .then(({ count, rows }) => {
+      return {
+        totalRecipeCuisines: count,
+        recipeCuisines: rows.map((e) => toResponseFormat(e.toJSON())),
+      };
+    })
+    .then(({ totalRecipeCuisines, recipeCuisines }) => {
+      const totalPages = Math.ceil(totalRecipeCuisines / pageSize);
+
       return res.status(success.ok).json({
         ok: true,
         recipeCuisines,
+        totalPages,
+        currentPage: page,
+        pageSize,
+        totalCount: totalRecipeCuisines,
       });
     })
     .catch((error) => {
