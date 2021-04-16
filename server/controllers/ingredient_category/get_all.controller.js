e2ce4bf872c8e8554ff22442f6e6900e0104a53d
jibaru <xxnacho1997xx@gmail.com>
2021-04-16 10:14:05 -0500

    Added filters

--- server/controllers/ingredient_category/get_all.controller.js
+++ server/controllers/ingredient_category/get_all.controller.js
@@ -1,20 +1,104 @@
 const { toResponseFormat } = require("../../utils/response_formatter");
 const { success, clientError } = require("../../utils/http_status_codes");
-const { IngredientCategory } = require("../../db/models/index");
+const {
+  IngredientCategory,
+  Sequelize,
+  Ingredient,
+} = require("../../db/models/index");
+const PaginationConstants = require("../../constants/pagination.contants");
 
 /// Get all IngredientCategories
 const getAllController = (req, res) => {
-  IngredientCategory.findAll()
-    .then((ingredientCategories) =>
-      ingredientCategories.map((e) => toResponseFormat(e.toJSON()))
-    )
+  const Op = Sequelize.Op;
+
+  const {
+    name,
+    ingredientIds,
+    fromCreatedAt,
+    toCreatedAt,
+    page = PaginationConstants.DEFAULT_PAGE,
+    pageSize = PaginationConstants.PAGE_SIZE,
+  } = req.query;
+
+  IngredientCategory.findAll({
+    attributes: {
+      include: ["createdAt"],
+    },
+    include: [
+      {
+        model: Ingredient,
+        as: "ingredients",
+        attributes: {
+          include: ["id"],
+        },
+      },
+    ],
+    where: {
+      // Filter name%
+      ...(name && {
+        name: {
+          [Op.startsWith]: name,
+        },
+      }),
+      // Filter from X to Y createdAt
+      ...((fromCreatedAt || toCreatedAt) && {
+        createdAt: {
+          ...(fromCreatedAt && {
+            [Op.gte]: fromCreatedAt,
+          }),
+          ...(toCreatedAt && {
+            [Op.lte]: toCreatedAt,
+          }),
+        },
+      }),
+    },
+    order: [["createdAt", "DESC"]],
+  })
     .then((ingredientCategories) => {
+      // Check if an ingredientCategory has all ingredientIds
+      // If ingredientIds is empty or not exists, return true
+      function isInAllIngredients(ingredients) {
+        if (!ingredientIds) {
+          return true;
+        }
+        const queryIngredientIds = ingredients.map(({ id }) => id);
+        return ingredientIds.every((ingredientId) =>
+          queryIngredientIds.includes(ingredientId)
+        );
+      }
+
+      const filteredIngredientCategories = ingredientCategories.filter((ic) =>
+        isInAllIngredients(ic.ingredients)
+      );
+
+      const start = page * pageSize - pageSize;
+      const end = start + pageSize;
+
+      return {
+        count: filteredIngredientCategories.length,
+        rows: filteredIngredientCategories.slice(start, end),
+      };
+    })
+    .then(({ count, rows }) => {
+      return {
+        totalIngredientCategories: count,
+        ingredientCategories: rows.map((e) => toResponseFormat(e.toJSON())),
+      };
+    })
+    .then(({ totalIngredientCategories, ingredientCategories }) => {
+      const totalPages = Math.ceil(totalIngredientCategories / pageSize);
+
       return res.status(success.ok).json({
         ok: true,
         ingredientCategories,
+        totalPages,
+        currentPage: page,
+        pageSize,
+        totalCount: totalIngredientCategories,
       });
     })
     .catch((error) => {
+      console.log(error);
       return res.status(clientError.badRequest).json({
         ok: false,
         error,
