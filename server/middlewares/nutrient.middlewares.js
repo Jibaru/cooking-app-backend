c6879faec62c269ec40f65842780444e75a64a79
jibaru <xxnacho1997xx@gmail.com>
2021-04-16 11:27:46 -0500

    Added getAllNutrientsMiddleware

--- server/middlewares/nutrient.middlewares.js
+++ server/middlewares/nutrient.middlewares.js
@@ -40,8 +40,46 @@ const getOneNutrientMiddleware = checkSchema({
   },
 });
 
+const getAllNutrientsMiddleware = checkSchema({
+  name: {
+    optional: true,
+    trim: true,
+  },
+  ingredientIds: {
+    optional: true,
+    isArray: true,
+    toArray: true,
+    customSanitizer: {
+      options: (value) => {
+        return value.map((e) => parseInt(e));
+      },
+    },
+  },
+  fromCreatedAt: {
+    optional: true,
+    trim: true,
+    toDate: true,
+  },
+  toCreatedAt: {
+    optional: true,
+    trim: true,
+    toDate: true,
+  },
+  page: {
+    optional: true,
+    trim: true,
+    toInt: true,
+  },
+  pageSize: {
+    optional: true,
+    trim: true,
+    toInt: true,
+  },
+});
+
 module.exports = {
   createNutrientMiddleware,
   deleteNutrientMiddleware,
   getOneNutrientMiddleware,
+  getAllNutrientsMiddleware,
 };
