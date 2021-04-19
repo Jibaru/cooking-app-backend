1432ee09cd9bc5201c4a121c3a7e5a66ecef0dc8
jibaru <xxnacho1997xx@gmail.com>
2021-04-18 19:09:24 -0500

    Removed instructionId

--- server/controllers/step/get_one.controller.js
+++ server/controllers/step/get_one.controller.js
@@ -8,7 +8,7 @@ const getOneController = (req, res) => {
 
   Step.findByPk(id, {
     attributes: {
-      exclude: ["instructionId", "stepImageId"],
+      exclude: ["stepImageId"],
     },
     include: [
       {
